98a282294869d0ce05a9b6be97d4e26d3e11ea89
Rushi <rushikesh.swami@kanakasoftware.com>
2024-03-04 18:31:08 +0530

    skills drop down is remaining

--- js/home.js
+++ js/home.js
@@ -1,4 +1,298 @@
-let user = JSON.parse(localStorage.getItem("user"));
-let username = user.userName;
-let usernameID = document.getElementById("usernameID");
-usernameID.innerHTML = "<h1>" + username + ",</h1>";
+//show box
+let viewemail = document.getElementById("viewemail");
+let viewusername = document.getElementById("viewusername");
+let viewfirstname = document.getElementById("viewfirstname");
+let viewlastname = document.getElementById("viewlastname");
+let viewphone = document.getElementById("viewphone");
+let viewphonealter = document.getElementById("viewphonealter");
+let viewlanguage = document.getElementById("viewlanguage");
+let viewdob = document.getElementById("viewdob");
+let viewgender = document.getElementById("viewgender");
+let viewskills = document.getElementById("viewskills");
+let viewroles = document.getElementById("viewroles");
+let viewaddress = document.getElementById("viewaddress");
+
+let viewrole = document.getElementById("role");
+let viewread = document.getElementById("viewread");
+let viewwrite = document.getElementById("viewwrite");
+let viewupdate = document.getElementById("viewupdate");
+
+//edit box
+let email = document.getElementById("email");
+let username = document.getElementById("username");
+let firstname = document.getElementById("firstname");
+let lastname = document.getElementById("lastname");
+let phone = document.getElementById("phone");
+let phonealter = document.getElementById("phonealter");
+let language = document.getElementById("language");
+let dob = document.getElementById("dob");
+let skills = document.getElementById("skills");
+let address = document.getElementById("address");
+
+let admin = document.getElementById("admin");
+let readcheck = document.getElementById("readcheck");
+let writecheck = document.getElementById("writecheck");
+let updatecheck = document.getElementById("updatecheck");
+let read = document.getElementById("read");
+let write = document.getElementById("write");
+let update = document.getElementById("update");
+
+//local changes
+let userindex = localStorage.getItem("currentuser");
+let usersData = JSON.parse(localStorage.getItem("usersData"));
+let currentuser = usersData[userindex];
+
+viewemail.innerText = currentuser.email;
+viewusername.innerText = currentuser.username;
+viewfirstname.innerText = currentuser.firstname;
+viewlastname.innerText = currentuser.lastname;
+viewphone.innerText = currentuser.phone;
+if(currentuser.phonealter) viewphonealter.innerText = currentuser.phonealter;
+else viewphonealter.innerHTML = "<p style=''>Yet not set</p>";
+// viewlanguage.innerText = currentuser.language;
+viewdob.innerText = currentuser.dob;
+viewgender.innerText = currentuser.gender;
+viewaddress.innerText = currentuser.address;
+let skillList = currentuser.programmingSkills;
+let skillFull = "   ";
+console.log(skillList);
+skillList.forEach((skill,index) => {
+    skillFull = skillFull + "&#8226; " + skill +" ";
+});
+viewskills.innerHTML = skillFull;
+
+//edit local change
+email.value = currentuser.email;
+username.value = currentuser.username;
+firstname.value = currentuser.firstname;
+lastname.value = currentuser.lastname;
+phone.value = currentuser.phone;
+phonealter.value = currentuser.phonealter;
+language.value = currentuser.language;
+dob.value = currentuser.dob;
+document.getElementById(currentuser.gender.toLowerCase()).checked = true;
+address.value = currentuser.address;
+// skills.value = currentuser.skills;
+roles = currentuser.roles;
+console.log(roles);
+if(roles.admin){
+    console.log("admin",roles.admin);
+    // admin.innerText = 'Admin';
+    document.getElementById("role").innerText = "Admin Roles";
+    read.innerText = 'Read';
+    write.innerText = 'Write';
+    update.innerText = 'Update';
+
+    viewread.innerHTML = "&#8226; Read";
+    viewwrite.innerHTML = "&#8226; Write";
+    viewupdate.innerHTML = "&#8226; Update";
+    
+    viewrole.innerText = "Admin Roles";
+    if(roles.read){
+        readcheck.checked = true;
+        viewread.style.display = 'block';
+    }
+    else{
+        readcheck.checked = false;
+        viewread.style.display = 'none';
+    }
+    if(roles.write){
+        writecheck.checked = true;
+        viewwrite.style.display = 'block';
+    }
+    else {
+        writecheck.checked = false;
+        viewwrite.style.display = 'none';
+    }
+    if(roles.update){
+        updatecheck.checked = true;
+        viewupdate.style.display = 'block';
+    }
+    else{
+        updatecheck.checked = false;
+        viewupdate.style.display = 'none';
+    }
+}
+if(roles.user){
+    console.log("user",roles.user);
+    // admin.innerText = 'Admin';
+    let roleedit = document.getElementById("role-edit");
+    read.innerText = 'Read';
+    write.innerText = 'Update';
+    update.innerText = 'Execute';
+    readcheck.disabled = true;
+    writecheck.disabled = true;
+    updatecheck.disabled = true;
+
+    viewread.innerHTML = "&#8226; Read";
+    viewwrite.innerHTML = "&#8226; Update";
+    viewupdate.innerHTML = "&#8226; Execute";
+    
+    viewrole.innerText = "User Roles";
+    if(roles.user.includes('read')){
+        readcheck.checked = true;
+        viewread.style.display = 'block';
+    }
+    else{
+        readcheck.checked = false;
+        viewwrite.style.display = 'none';
+    }
+    if(roles.user.includes('update')){
+        writecheck.checked = true;
+        viewwrite.style.display = 'block';
+    }
+    else {
+        // writecheck.checked = false;
+        viewwrite.style.display = 'none';
+    }
+    if(roles.user.includes('execute')){
+        updatecheck.checked = true;
+        viewupdate.style.display = 'block';
+    }
+    else{
+        update.checked = false;
+        viewupdate.style.display = 'none';
+    }
+    // roleedit.style.display = 'none';
+}
+// console.log(roles.user);
+
+function showSection(sectionId) {
+    var navItems = document.querySelectorAll('.nav-item');
+    navItems.forEach(function(item) {
+        item.classList.remove('active-nav-item');
+    });
+    
+    var clickedNavItem = document.querySelector('[onclick="showSection(\'' + sectionId + '\')"]');
+    if (clickedNavItem) {
+        clickedNavItem.parentElement.classList.add('active-nav-item');
+    }
+    var sections = document.querySelectorAll('.section');
+    sections.forEach(function(section) {
+        section.classList.remove('active');
+    });
+    var selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+        selectedSection.classList.add('active');
+    }
+}
+
+
+programmingSkills = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "C++",
+    "HTML",
+    "CSS",
+    "React",
+    "Angular",
+    "Node.js",
+    "Ruby",
+    "PHP",
+    "Swift",
+    "TypeScript",
+    "Go",
+    "SQL",
+    "Rust"
+];
+autocomplete(document.getElementById("userSkills"), programmingSkills, "selectedSkillsContainer");
+
+function autocomplete(inp, arr, containerId) {
+    var currentFocus;
+
+    inp.addEventListener("input", function(e) {
+        var val = this.value;
+        closeAllLists();
+
+        if (!val) {
+            return false;
+        }
+
+        currentFocus = -1;
+        var a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        this.parentNode.appendChild(a);
+
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                var b = document.createElement("DIV");
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+
+                b.addEventListener("click", function(e) {
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    closeAllLists();
+                
+                    // Clear the input box after selecting an item
+                    inp.value = "";
+                
+                    // Add the selected skill to the container:
+                    const selectedSkillsContainer = document.getElementById(containerId);
+                    const selectedSkillItem = document.createElement("div");
+                    selectedSkillItem.classList.add("selected-skill-item");
+                    selectedSkillItem.textContent = this.getElementsByTagName("input")[0].value;
+                
+                    const closeButton = document.createElement("span");
+                    closeButton.classList.add("close-button");
+                    closeButton.innerHTML = "&times;";
+                    closeButton.addEventListener("click", function() {
+                        selectedSkillsContainer.removeChild(selectedSkillItem);
+                    });
+                
+                    selectedSkillItem.appendChild(closeButton);
+                    selectedSkillsContainer.appendChild(selectedSkillItem);
+                });
+
+                a.appendChild(b);
+            }
+        }
+    });
+
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+
+        if (e.keyCode == 40) {
+            currentFocus++;
+            addActive(x);
+        } else if (e.keyCode == 38) {
+            currentFocus--;
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            e.preventDefault();
+            if (currentFocus > -1) {
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+
+    function addActive(x) {
+        if (!x) return false;
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+
+    function removeActive(x) {
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+
+    document.addEventListener("click", function(e) {
+        closeAllLists(e.target);
+    });
+}
--- js/index.js
+++ js/index.js
@@ -13,13 +13,13 @@ let emailAlert = document.getElementById("emailAlert");
 let passwordAlert = document.getElementById("passwordAlert");
 let toast = document.getElementById('toast');
 
-// let storedEmail = "", storedPassword="";
-// if (user) {
-//     storedEmail = user.userId.length;
-//     storedPassword = user.userPassword;
-// } else {
-//     console.log("User not found in localStorage");
-// }
+let forgotEmail = document.getElementById("forgotEmail");
+let newPassword = document.getElementById("newPassword");
+let confirmPassword = document.getElementById("confirmPassword");
+let forgotEmailalert = document.getElementById("forgotEmailalert");
+let fogotPasswordalert = document.getElementById("fogotPasswordalert");
+let forgotConfirmPasswordalert = document.getElementById("forgotConfirmPasswordalert");
+
 
 let loginbtn = document.getElementById("btnSignIn");
 loginbtn.disabled = true;
@@ -53,6 +53,7 @@ function login(event){
     }
 
     let validemail = false, validpassword = false;
+    let currentuser = 0;
     if(usersdata.length!==0){
         usersdata.forEach((user,index)=> {
             const userEmail = user.email;
@@ -61,6 +62,7 @@ function login(event){
             if(userEmail===currentEmail.value || userUsername===currentEmail.value){
                 validemail = true;
                 if(userPassword===currentPassword.value){
+                    currentuser = index;
                     validpassword = true;
                     // break;
                 }
@@ -106,7 +108,8 @@ function login(event){
         // toast for password
     }
     else {
-        
+        console.log(currentuser);
+        localStorage.setItem("currentuser",currentuser);
         emailAlert.innerText = "";
         emailAlert.style.display = 'none'; 
         passwordAlert.innerText = "";
@@ -122,6 +125,159 @@ function login(event){
         // toast sucesfull
     }
 }
+
+function forgotPassword(){
+
+}
+function openModal() {
+    document.getElementById('forgotPasswordModal').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('forgotPasswordModal').style.display = 'none';
+}
+
+function resetPassword() {
+    // Add your password reset logic here
+    let forgotemail = forgotEmail.value;
+    let newpassword =  newPassword.value;
+    let confirmpassword = confirmPassword.value;
+    let valid = false;
+    // valid = isValidEmail(forgotemail);
+    index = isEmailRegistered(forgotemail);
+    if(index===-1){
+        // updateTextError(fogotPasswordalert,"Email is not registered",true);
+        console.log(index);
+        return;
+    }
+    else{
+        console.log(index);
+        // updateTextError(fogotPasswordalert,"",false);
+    }
+    valid = isValidPass(newpassword);
+    valid = isValidCpass(newpassword,confirmpassword);
+    if(!valid){
+        return;
+    }
+    usersdata[index].password = newpassword;
+    localStorage.setItem("usersData", JSON.stringify(usersdata));
+    closeModal();
+    toast.classList.remove('hidden');
+    toast.classList.add('success');
+    toast.classList.remove('error');
+    toast.textContent = "Password reset successful!";
+    setTimeout(() => {
+        toast.classList.add('hidden');
+    },3000);
+}
+
+// Close modal if clicked outside of it
+window.onclick = function (event) {
+    var modal = document.getElementById('forgotPasswordModal');
+    if (event.target == modal) {
+        closeModal();
+    }
+}
+function isValidPass(passVal){
+    
+    if(passVal.length==0){
+        updateTextError(fogotPasswordalert,"Password is required",true);
+        newPassword.style.border = "1px solid red";
+        return false;
+    } 
+    else if(passVal.length<8){
+        updateTextError(fogotPasswordalert,"Password must be at least 8 characters long  ",true);
+        newPassword.style.border = "1px solid red";
+        return false;
+    }
+    else if((/\s/.test(passVal))) {
+        updateTextError(fogotPasswordalert,"Invalid Password. Spaces are not allowed",true);
+        newPassword.style.border = "1px solid red";
+        return false;
+    }
+
+    let capital = false, lower = false, number = false, sybmol = false, len = true;
+    for (let i = 0; i < passVal.length; i++) {
+        let c = passVal.charAt(i);
+        if (c >= 'A' && c <= 'Z') capital = true;
+        else if (c >= 'a' && c <= 'z') lower = true;
+        else if (c >= '0' && c <= '9') number = true;
+        else sybmol = true;
+        console.log(c + " " + capital + " " + lower + " " + number + " " + sybmol);
+    }
+    console.log(capital + " " + lower + " " + number + " " + sybmol);
+
+    if (!capital || !lower || !number || !sybmol || !len) {
+        updateTextError(fogotPasswordalert,"Password must include at least one lowercase letter, one uppercase letter, one number, and one symbol",true);
+        newPassword.style.border = "1px solid red";
+        return false;
+    }
+    else{
+        updateTextError(fogotPasswordalert,"",false);
+        newPassword.style.border = "0px solid red";
+        return true;
+    }
+}
+function isValidCpass(passVal,cpassVal){
+    cpassVal = cpassVal.trim();
+    passVal = passVal.trim();
+
+    if(cpassVal.length === 0) updateTextError(forgotConfirmPasswordalert,"Confirm password is required",true);
+    else if(cpassVal!==passVal) updateTextError(forgotConfirmPasswordalert,"Password didn't match",true);
+    else{
+        confirmPassword.style.border = "0px solid red";
+        updateTextError(forgotConfirmPasswordalert,"",false);
+        return true;
+    }
+    confirmPassword.style.border = "1px solid red";
+    return false;
+}
+function isValidEmail(emailVal) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (forgotEmailalert.length === 0) {
+        updateTextError(forgotEmailalert, "Email is required", true);
+    } else if (!emailRegex.test(emailVal)) {
+        updateTextError(forgotEmailalert, "Invalid email format", true);
+    } else {
+        forgotEmail.style.border = "0px solid red";
+        updateTextError(forgotEmailalert, "", false);
+        return true;
+    }
+
+    forgotEmail.style.border = "1px solid red";
+    return true;
+}
+function isEmailRegistered(emailVal) {
+    for (let index = 0; index < usersdata.length; index++) {
+        const user = usersdata[index];
+        const userEmail = user.email;
+        const userUsername = user.username;
+
+        console.log(emailVal, " ", userUsername);
+        if (userEmail === emailVal || userUsername === emailVal) {
+            forgotEmail.style.border = "0px solid red";
+            updateTextError(forgotEmailalert, "", false);
+            return index; // Break the loop and return the index
+        }
+    }
+
+    console.log(emailVal);
+    forgotEmail.style.border = "1px solid red";
+    updateTextError(forgotEmailalert, "Email is not registered", true);
+
+    return -1; // Return -1 if no match is found
+}
+
+function updateTextError(element,msg,state){
+    if(state){
+        element.style.display = 'block';
+        element.innerText = msg; 
+    }else{
+        element.style.display = 'none';
+    }
+}
+
 
 // const notifications = document.querySelector(".notifications");
 
